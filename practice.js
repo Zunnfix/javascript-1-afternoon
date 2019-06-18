62681b50ea6fc4626c561f073062ade346eba481
Chase Shields <cwshields2@gmail.com>
2019-06-18 09:51:13 -0500

    added semicolons

--- practice.js
+++ practice.js
@@ -2,31 +2,31 @@
 
 //Create a variable called myName that is a string data type
 
-let myName = "Chase"
+let myName = "Chase";
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-let myAge = 0
+let myAge = 0;
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-let lovesCode = true
+let lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-let greatestFear
+let greatestFear;
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
-let devMountainGoal = null
+let devMountainGoal = null;
 
 //////////////////PROBLEM 6////////////////////
 
@@ -36,7 +36,7 @@ let devMountainGoal = null
 //plus the value of the name parameter.
 
 function greeting(name) {
-  return "Hello, " + name
+  return "Hello, " + name;
 }
 
 //////////////////PROBLEM 7////////////////////
@@ -45,7 +45,7 @@ function greeting(name) {
 //Name it newGreeting.
 
 function newGreeting(name) {
-  return "Hello" + name
+  return "Hello" + name;
 }
 
 //////////////////PROBLEM 8////////////////////
@@ -53,7 +53,7 @@ function newGreeting(name) {
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-let = ["apples", "milk", "eggs", "bread"]
+let = ["apples", "milk", "eggs", "bread"];
 
 //////////////////PROBLEM 9////////////////////
 
@@ -72,7 +72,7 @@ let dog = {
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
-let devMountainClassPet = Object.assign(dog, name)
+let devMountainClassPet = Object.assign(dog, name);
 
 //////////////////PROBLEM 10////////////////////
 
@@ -85,13 +85,13 @@ let devMountainClassPet = Object.assign(dog, name)
 
 function nameCheck(name){
   if (name == 'Steven') {
-    console.log('What\'s up Steven?')
+    console.log('What\'s up Steven?');
   }
   else if (name == 'Bryan') {
-    console.log('Hey Bryan!')
+    console.log('Hey Bryan!');
   }
   else {
-    console.log('Cool name, ' + name + ".")
+    console.log('Cool name, ' + name + ".");
   }
 }
 
@@ -102,13 +102,13 @@ function nameCheck(name){
 // The add function should return the two parameters added together
 
 function add(n1, n2){
-  return n1 + n2
+  return n1 + n2;
 }
 
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
-let mathSum = add(3, 4)
+let mathSum = add(3, 4);
 
 //////////////////PROBLEM 12////////////////////
 
@@ -121,16 +121,16 @@ let mathSum = add(3, 4)
 
 function faveColorFinder(color){
   if (color == 'red') {
-    console.log('Red is a great color!')
+    console.log('Red is a great color!');
   }
   else if (color == 'green') {
-    console.log('Green is a solid favorite color!')
+    console.log('Green is a solid favorite color!');
   }
   else if (color == 'black') {
-    console.log('So trendy...')
+    console.log('So trendy...');
   }
   else {
-    console.log('You need to evaluate your favorite color choice.')
+    console.log('You need to evaluate your favorite color choice.');
   }
 }
 
@@ -171,13 +171,13 @@ let pondScope = ["realDuck"];
 
 //Create a variable called age with your age assigned to you
 
-let age = 26
+let age = 26;
 
 // FLASH FORWARD TO NEXT YEAR
 // reassign the value of age to be one greater than it was, because, we all get older
 
-age++
+age++;
 
 // Good news! We can live forever. Set your age to 999
 
-age = 999
+age = 999;
